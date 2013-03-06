39c413c11118344584d18d6f1c8dab614b14f619
htchoi1995 <htchoi1995@gmail.com>
2013-03-06 17:37:28 +0900

    STATE is now STYLE

--- src/com/aokp/romcontrol/fragments/PowerMenu.java
+++ src/com/aokp/romcontrol/fragments/PowerMenu.java
@@ -85,7 +85,7 @@ public class PowerMenu extends AOKPPreferenceFragment implements OnPreferenceCha
         PreferenceScreen prefSet = getPreferenceScreen();
         mExpandedDesktopPref = (ListPreference) prefSet.findPreference(PREF_EXPANDED_DESKTOP);
         mExpandedDesktopPref.setOnPreferenceChangeListener(this);
-        int expandedDesktopValue = Settings.System.getInt(getContentResolver(), Settings.System.EXPANDED_DESKTOP_STATE, 0);
+        int expandedDesktopValue = Settings.System.getInt(getContentResolver(), Settings.System.EXPANDED_DESKTOP_STYLE, 0);
         mExpandedDesktopPref.setValue(String.valueOf(expandedDesktopValue));
         updateExpandedDesktopSummary(expandedDesktopValue);
                 
@@ -204,7 +204,7 @@ public class PowerMenu extends AOKPPreferenceFragment implements OnPreferenceCha
         if (preference == mExpandedDesktopPref) {
             int expandedDesktopValue = Integer.valueOf((String) newValue);
             Settings.System.putInt(getContentResolver(),
-                    Settings.System.EXPANDED_DESKTOP_STATE, expandedDesktopValue);
+                    Settings.System.EXPANDED_DESKTOP_STYLE, expandedDesktopValue);
             updateExpandedDesktopSummary(expandedDesktopValue);
             return true;
         } else if (preference == mScreenshotDelay) {
