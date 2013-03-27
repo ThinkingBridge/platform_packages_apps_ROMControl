0681ef6ea26de001df77b15aaaecca5126ab3774
htchoi1995 <htchoi1995@gmail.com>
2013-03-27 19:07:06 +0900

    ROMControl: Vibration, NavRing, Lockscreen Arrows
    http://github.com/AOKP/packages_apps_ROMControl/commit/9c15509977b5d1da7
    8d020e7aed66e95cf00029a.

--- src/com/aokp/romcontrol/fragments/Lockscreens.java
+++ src/com/aokp/romcontrol/fragments/Lockscreens.java
@@ -31,6 +31,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceGroup;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
